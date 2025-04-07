e5f6113113b6916190119576d4f97ec71c1385bf
Cube <>
2025-04-08 00:49:29 +0200

    Specify Chrome for Playwright browser installation and remove compression level from PDF artifact upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex h-svh flex-col gap-2 font-medium">
+<div class="flex flex-col gap-2 font-medium">
 	<h1 class="text-4xl font-black">Report</h1>
 
 	<p class="text-lg">
@@ -8,7 +8,7 @@
 	</p>
 
 	{#each Array.from({ length: 100 }) as _, i}
-		<p class="text-base">Spam</p>
+		<p class="text-base">Heeyy</p>
 	{/each}
 
 	<h1 class="break-before-page text-2xl font-black">New Page</h1>
