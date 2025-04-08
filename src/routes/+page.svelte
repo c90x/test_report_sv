9c49e27a86d7cf994976590b6060d7cfcf97bbeb
# <e.0xcc3@gmail.com>
2025-04-08 12:04:17 +0200

    ✨ Update table structure and styling in report page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,9 @@
 	<h1 class="text-4xl font-black">Report</h1>
 
 	<p class="text-lg">
-		New<br/> 
-		890348659389034865938903486593890348659389034865938903486593890348659389034865938903486593890348659389034865938903486593890348659389034865938903486593<br/> 
+		New<br />
+		890348659389034865938903486593890348659389034865938903486593890348659389034865938903486593890348659389034865938903486593890348659389034865938903486593<br
+		/>
 		site
 	</p>
 
@@ -14,23 +15,51 @@
 	<h1 class="break-before-page text-2xl font-black">New Page</h1>
 	<p class="text-lg">Yeey!</p>
 
-	<div class="overflow-hidden rounded-xl border border-gray-700 shadow-md">
-		<table class="min-w-full divide-y divide-gray-700">
+	<div class="my-2 overflow-hidden rounded-xl border border-gray-700 shadow-md">
+		<table class="min-w-full divide-y divide-gray-700 text-base table-fixed">
+			<colgroup>
+				<col class="w-auto" />
+				{#each Array.from({ length: 3 }) as _}
+					<col class="w-1/4" />
+				{/each}
+			</colgroup>
 			<thead>
 				<tr>
-					<th scope="col" class="bg-gray-800 px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-300">Factors</th>
-					<th scope="col" class="bg-gray-800 px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-300">Germany</th>
-					<th scope="col" class="bg-gray-800 px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-300">USA</th>
-					<th scope="col" class="bg-gray-800 px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-300">Canada</th>
+					<th
+						scope="col"
+						class="bg-gray-800 px-6 py-3 text-left text-sm font-medium tracking-wider text-gray-300 uppercase"
+					>
+						Factors
+					</th>
+					<th
+						scope="col"
+						class="bg-gray-800 px-6 py-3 text-left text-sm font-medium tracking-wider text-gray-300 uppercase"
+					>
+						Germany
+					</th>
+					<th
+						scope="col"
+						class="bg-gray-800 px-6 py-3 text-left text-sm font-medium tracking-wider text-gray-300 uppercase"
+					>
+						USA
+					</th>
+					<th
+						scope="col"
+						class="bg-gray-800 px-6 py-3 text-left text-sm font-medium tracking-wider text-gray-300 uppercase"
+					>
+						Canada
+					</th>
 				</tr>
 			</thead>
 			<tbody class="divide-y divide-gray-700 bg-gray-900">
 				{#each Array.from({ length: 5 }) as _, i}
-					<tr class={`${i % 2 === 0 ? 'bg-gray-900' : 'bg-gray-800'} transition-all duration-200 ease-in-out hover:bg-blue-900`}>
-						<td class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-100">Factor{i + 1}</td>
-						<td class="whitespace-nowrap px-6 py-4 text-sm text-gray-300">Data</td>
-						<td class="whitespace-nowrap px-6 py-4 text-sm text-gray-300">Data</td>
-						<td class="whitespace-nowrap px-6 py-4 text-sm text-gray-300">Data</td>
+					<tr class={`${i % 2 === 0 ? 'bg-gray-900' : 'bg-gray-800'}`}>
+						<td class="px-6 py-4 font-medium whitespace-nowrap text-gray-100">
+							Factor{i + 1}
+						</td>
+						<td class="px-6 py-4 whitespace-nowrap text-gray-300">Data</td>
+						<td class="px-6 py-4 whitespace-nowrap text-gray-300">Data</td>
+						<td class="px-6 py-4 whitespace-nowrap text-gray-300">Data</td>
 					</tr>
 				{/each}
 			</tbody>
